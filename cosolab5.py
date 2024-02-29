b1cb779107f7fda00c2fa2ac8f4fc209b0246f97
brokie636 <alejooo636@gmail.com>
2024-02-29 17:03:42 -0500

    😄👍
    ya

--- cosolab5.py
+++ cosolab5.py
@@ -8,10 +8,10 @@ for root, dirs, files in os.walk(dir_path):
         if file.endswith('.txt'):
             with open(os.path.join(root, file), 'r') as f:
                 phrase = f.read()
-                
                 sentiment = os.path.basename(root)
-                df1 = df1._append({'phrase': phrase, 'sentiment': sentiment}, ignore_index=True)
-                
+                df1 = df1._append({'phrase': phrase, 'sentiment': sentiment}, ignore_index=True)            
+df1.to_csv('test_dataset.csv', index=False)
+
 df2 = pd.DataFrame(columns=['phrase', 'sentiment'])
 dir_path = 'train'               
 for root, dirs, files in os.walk(dir_path):
@@ -21,7 +21,4 @@ for root, dirs, files in os.walk(dir_path):
                 phrase = f.read()
                 sentiment = os.path.basename(root)
                 df2 = df2._append({'phrase': phrase, 'sentiment': sentiment}, ignore_index=True)
-
-df1.to_csv('test_dataset.csv', index=False)
 df2.to_csv('train_dataset.csv', index=False)
-#a
